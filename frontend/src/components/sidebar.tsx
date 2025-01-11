caf4dfb86a77c74d67c8b358477dfcba9799faf5
Sanchari Mandal <113378204+Hul0@users.noreply.github.com>
2025-01-11 16:12:40 +0530

    Refactor theme toggle and sidebar components; add sidebar links and improve H2 component for className support

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -5,37 +5,82 @@ import {
     HeartPulse,
     Settings,
 } from "lucide-react";
+import H2 from "./typography/H2";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./ModeToggle";
 
 const Sidebar = () => {
     return (
-        <div className="bg-accent h-full w-full max-w-64 space-y-4 p-8">
-            <h1 className="text-primary text-2xl font-bold">Organa</h1>
+        <div className="bg-accent flex h-full w-full max-w-64 flex-col justify-between p-8">
+            <div className="w-full space-y-4">
+                <H2 className="text-primary">Organa</H2>
+                <nav className="flex flex-col gap-4">
+                    {sidebarLinks.map(({ icon, text, path }) => (
+                        <SidebarLink
+                            key={text}
+                            text={text}
+                            path={path}
+                            icon={React.createElement(icon)}
+                        ></SidebarLink>
+                    ))}
+                </nav>
+            </div>
             <div className="flex flex-col gap-4">
-                <div className="flex gap-2">
-                    <LayoutDashboard></LayoutDashboard>
-                    Dashboard
-                </div>
-                <div className="flex gap-2">
-                    <HeartPulse></HeartPulse>
-                    Organ
-                </div>
-                <div className="flex gap-2">
-                    <Bell></Bell>
-                    Matches
-                </div>
-                <div className="flex gap-2">
-                    <ClipboardMinus></ClipboardMinus>
-                    Patient
-                </div>
-                <div className="flex gap-2">
-                    <Settings></Settings>
-                    Settings
-                </div>
+                <ModeToggle />
+                <Button>Logout</Button>
             </div>
         </div>
     );
 };
+
 export default Sidebar;
+
+const SidebarLink = ({
+    icon,
+    text,
+    path,
+}: {
+    icon?: React.ReactNode;
+    text: string;
+    path: string;
+}) => {
+    return (
+        <Link href={path ? path : "/"} className="flex w-full gap-2 text-left">
+            {icon}
+            {text}
+        </Link>
+    );
+};
+
+const sidebarLinks = [
+    {
+        icon: LayoutDashboard,
+        text: "Dashboard",
+        path: "/dashboard",
+    },
+    {
+        icon: HeartPulse,
+        text: "Organ",
+        path: "/organ",
+    },
+    {
+        icon: Bell,
+        text: "Matches",
+        path: "/matches",
+    },
+    {
+        icon: ClipboardMinus,
+        text: "Patient",
+        path: "/patient",
+    },
+    {
+        icon: Settings,
+        text: "Settings",
+        path: "/settings",
+    },
+];
 
 /*
 Top - Logo & Name
