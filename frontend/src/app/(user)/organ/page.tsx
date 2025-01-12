8c1729604ab9cee3e2c2153e2e89723c8112d7f4
Sanchari Mandal <113378204+Hul0@users.noreply.github.com>
2025-01-12 16:40:30 +0530

    Add charts for organ availability and distribution; update OrganPage layout and patient data

--- frontend/src/app/(user)/organ/page.tsx
+++ frontend/src/app/(user)/organ/page.tsx
@@ -1,4 +1,29 @@
+import AvailableMatchedTransplantedChart from "@/components/charts/AvailableMatchedTransplantedChart";
+import { OrganDistribution } from "@/components/charts/OrganDistribution";
+import H2 from "@/components/typography/H2";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+// import OrganDistribution from "@/components/charts/OrganDistribution";
+
 const OrganPage = () => {
-    return <div>OrganPage</div>;
+    return (
+        <div className="py-4">
+            <div className="mb-4 flex justify-between">
+                <H2>Organ</H2>
+                <Button asChild>
+                    <Link href="/organ/add">Add Available Organ</Link>
+                </Button>
+                {/* <Bell size={36} className="text-primary" /> */}
+            </div>
+            <div className="flex gap-4">
+                <div className="basis-1/2 rounded-lg border p-4 shadow-sm">
+                    <AvailableMatchedTransplantedChart />
+                </div>
+                <div className="basis-1/2 rounded-lg border p-4 shadow-sm">
+                    <OrganDistribution />
+                </div>
+            </div>
+        </div>
+    );
 };
 export default OrganPage;
