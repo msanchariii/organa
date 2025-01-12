fdfa8eb5e02c46e43075d9b9be6fa5a8741f2b73
Sanchari Mandal <113378204+Hul0@users.noreply.github.com>
2025-01-12 14:39:56 +0530

    Refactor MatchCard component @kankanmondal22

--- frontend/src/components/match-card.tsx
+++ frontend/src/components/match-card.tsx
@@ -10,11 +10,21 @@ import {
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+// * Remove all padding first. Read all the instructions then follow it. Add padding (if needed) at last.
+
 const MatchCard = () => {
     return (
+        // @kankanmondal22
+        // ! The outermost div is not needed. DO NOT INCLUDE ANY ELEMENT IF IT IS NOT NEEDED
         <div>
             <Card className="p-8">
                 <div className="flex">
+                    {/* 
+                    //! Card component should not directly contain any other component other than CardHeader, CardContent, CardFooter (DO NOT USE div or Avatar directly inside Card)
+                    // TODO: Remove div and Avatar from here
+                    // TODO: Use only CardHeader, CardContent, CardFooter components instead of div
+                    // * Avatar component should be used inside CardHeader component (use flex, flex-row in CardHeader to align Avatar and CardTitle)
+                    */}
                     <Avatar>
                         <AvatarImage src="https://github.com/shadcn.png" />
                         <AvatarFallback>CN</AvatarFallback>
@@ -27,16 +37,26 @@ const MatchCard = () => {
                         <CardDescription>Organ id- 3000000</CardDescription>
                     </CardHeader>
                 </div>
+                {/* 
+                //! Remove div
+                */}
                 <div className="px-10 py-4">
                     <CardContent>
                         <CardDescription>
+                            {/* 
+                            // ! CardDescription should be inside CardHeader
+                            // * you can use any html element inside CardContent such as h1, h2, p etc. and add tailwind classes accordingly 
+                            */}
                             Approval need for dispatch
                         </CardDescription>
                     </CardContent>
 
                     <CardFooter className="space-x-4 py-0">
-                        <Button variant={"outline"}>Approve</Button>
-                        <Button>Dismiss</Button>
+                        {/* 
+                            // * Use primary(default) button for approve and secondary button for dismiss
+                        */}
+                        <Button variant={"default"}>Approve</Button>
+                        <Button variant={"secondary"}>Dismiss</Button>
                     </CardFooter>
                 </div>
             </Card>
