06a3d6794fe16b1fa4e3d74007e5515932fe1f7f
kankanmondal22 <kankanmondal22@gmail.com>
2025-01-13 13:30:49 +0530

    match-card

--- frontend/src/components/match-card.tsx
+++ frontend/src/components/match-card.tsx
@@ -9,6 +9,26 @@ import {
 } from "@/components/ui/card";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { DialogClose } from "@radix-ui/react-dialog";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
 
 // * Remove all padding first. Read all the instructions then follow it. Add padding (if needed) at last.
 
@@ -17,19 +37,23 @@ const MatchCard = () => {
         // @kankanmondal22
         // ! Add time of notification in the card i.e. "1 hours ago"
 
-        <Card className="bg-card/50 p-8">
+        <Card className="bg-card/50">
             {/* 
                     // * Avatar component should be used inside CardHeader component (use flex, flex-row in CardHeader to align Avatar and CardTitle)
                     */}
 
-            <CardHeader className="flex flex-row p-4 py-0">
-                <Avatar>
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <CardTitle className="text-lg">Patient match found</CardTitle>
-                <CardDescription>Organ id- 3000000</CardDescription>
-                <p className="items-between flex justify-between">1 hour ago</p>
+            <CardHeader className="flex flex-row justify-between">
+                <CardTitle className="flex flex-row items-center gap-4">
+                    <Avatar className="h-8 w-8 text-sm">
+                        <AvatarImage src="" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    Patient match found
+                </CardTitle>
+                <CardDescription className="flex items-center justify-center gap-2">
+                    <div className="bg-primary h-2 w-2 rounded"></div> 1 hour
+                    ago
+                </CardDescription>
             </CardHeader>
 
             {/*
@@ -37,13 +61,72 @@ const MatchCard = () => {
             <CardContent>
                 {/*
                  */}
-                <p> Approval need for dispatch</p>
+                <p className="text-sm">
+                    <strong>Recipient Name:</strong>Kankan Mondal
+                </p>
+                <p className="text-sm">
+                    <strong>Donor Name:</strong> Kankan Mondal
+                </p>
+                <p className="text-sm">
+                    <strong>Organ Type:</strong> Kidney
+                </p>
+                <p className="text-sm">
+                    <strong>Viable Time of Organ:</strong> 5 hours
+                </p>
+
+                <p className="text-sm">
+                    <strong>Organ Conditon:</strong> Good
+                </p>
             </CardContent>
 
-            <CardFooter className="space-x-4 py-0">
+            <CardFooter className="space-x-4">
                 {/* 
                             // * Use primary(default) button for approve and secondary button for dismiss
                         */}
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant={"outline"}>View Details</Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>
+                                Patient Name:Kankna MOndal
+                            </DialogTitle>
+                            <DialogDescription>
+                                Kidney Found for kankna
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div>
+                            <Table>
+                                <TableCaption>Details</TableCaption>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead className="w-[100px]"></TableHead>
+                                        <TableHead>Doner</TableHead>
+                                        <TableHead>Recipient</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>Blood group</TableCell>
+                                        <TableCell>B+</TableCell>
+                                        <TableCell>B+ </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>Blood group</TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </div>
+                        <DialogFooter className="sm:justify-start">
+                            <DialogClose asChild>
+                                <Button type="button" variant="secondary">
+                                    Close
+                                </Button>
+                            </DialogClose>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
                 <Button variant={"default"}>Approve</Button>
                 <Button variant={"secondary"}>Dismiss</Button>
             </CardFooter>
